b15dc18e5ee8599c2c4b65b098611ae7a2337edc
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-20 15:44:17 -0500

    add base path handling to config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
-export default async function (eleventyConfig) {
-  eleventyConfig.setOutputDirectory("dist");
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
+// run `BASE_PATH=/posts pnpm build` to demo prod
+const basePath = process.env.BASE_PATH || "";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
   eleventyConfig.ignores.add("README.md");
 }
+
+export const config = {
+  dir: {
+    output: "dist" + basePath,
+  },
+  pathPrefix: basePath + "/",
+};
