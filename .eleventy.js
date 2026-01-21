1862d7969e468fd789ed8976be0b34257ef9ced2
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-20 22:20:49 -0500

    remove eleventy-plugin-vite

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
-import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 
 // run `BASE_PATH=/posts pnpm build` to demo prod
 const basePath = process.env.BASE_PATH || "";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(HtmlBasePlugin);
-  eleventyConfig.addPlugin(EleventyVitePlugin);
 
   eleventyConfig.ignores.add("README.md");
 
