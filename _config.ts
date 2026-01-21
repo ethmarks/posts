3d76ce09ee33a94839c72aef4252783a93758711
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-20 22:03:53 -0500

    comment out unused Lume plugins

--- _config.ts
+++ _config.ts
@@ -1,24 +1,24 @@
 import lume from "lume/mod.ts";
-import date from "lume/plugins/date.ts";
-import code_highlight from "lume/plugins/code_highlight.ts";
-import lume_cms from "lume/plugins/lume_cms.ts";
-import esbuild from "lume/plugins/esbuild.ts";
-import lightningcss from "lume/plugins/lightningcss.ts";
-import base_path from "lume/plugins/base_path.ts";
-import metas from "lume/plugins/metas.ts";
-import feed from "lume/plugins/feed.ts";
-import sitemap from "lume/plugins/sitemap.ts";
+// import date from "lume/plugins/date.ts";
+// import code_highlight from "lume/plugins/code_highlight.ts";
+// import lume_cms from "lume/plugins/lume_cms.ts";
+// import esbuild from "lume/plugins/esbuild.ts";
+// import lightningcss from "lume/plugins/lightningcss.ts";
+// import base_path from "lume/plugins/base_path.ts";
+// import metas from "lume/plugins/metas.ts";
+// import feed from "lume/plugins/feed.ts";
+// import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume();
 
-site.use(date());
-site.use(code_highlight());
-site.use(lume_cms());
-site.use(esbuild());
-site.use(lightningcss());
-site.use(base_path());
-site.use(metas());
-site.use(feed());
-site.use(sitemap());
+// site.use(date());
+// site.use(code_highlight());
+// site.use(lume_cms());
+// site.use(esbuild());
+// site.use(lightningcss());
+// site.use(base_path());
+// site.use(metas());
+// site.use(feed());
+// site.use(sitemap());
 
 export default site;
