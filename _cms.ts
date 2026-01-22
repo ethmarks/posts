cf4f409af85639e512bd58ba9b3418d5b7cca391
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-21 21:58:37 -0500

    add cms config for storage, collection, and auth

--- _cms.ts
+++ _cms.ts
@@ -1,5 +1,33 @@
 import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
 
 const cms = lumeCMS();
+
+const token = Deno.env.get("GITHUB_TOKEN") || "";
+cms.storage("gh", GitHub.create("ethmarks/posts", token));
+
+const isLocal = !(Deno.env.get("DENO_DEPLOY"));
+const localStorage = "src:content/*.md";
+const remoteStorage = "gh:content/*.md";
+
+const storageToUse = isLocal ? localStorage : remoteStorage;
+
+cms.collection({
+  name: "posts",
+  store: storageToUse,
+  fields: [
+    "title: text",
+    "published: date",
+    "content: markdown",
+  ],
+});
+
+const password = Deno.env.get("CMS_PASS");
+if (!password) {
+  throw new Error("CMS_PASS environment variable is required");
+}
+cms.auth({
+  ethmarks: password,
+});
 
 export default cms;
